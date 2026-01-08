828649f432aea5c2b99f915b193b2a17c17fa0a0
revwo <90896714+ttvRevwo@users.noreply.github.com>
2026-01-08 08:54:55 +0100

    Button text
    Vissza a főoldalra átírása vissza-ra, hogy szép legyen

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -63,7 +63,7 @@ export default function Login() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} width={20} height={20}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
         </svg>
-        Vissza a főoldalra
+        Vissza
       </a>
       
       <h2 className="register-title">Bejelentkezés</h2>
